c01978a07cab766ef5a0ca41c6d5cd26a3050534
qpeterp <lsepeter0827@naver.com>
2024-08-21 09:34:59 +0900

    Fix :: Change file content LockScreen.kt <-> TodoScreen.kt

--- app/src/main/java/com/qpeterp/todolock/ui/main/ui/setting/SettingScreen.kt
+++ app/src/main/java/com/qpeterp/todolock/ui/main/ui/setting/SettingScreen.kt
@@ -8,8 +8,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
+@Preview(showBackground = true)
 @Composable
 fun SettingScreen() {
     Column(
