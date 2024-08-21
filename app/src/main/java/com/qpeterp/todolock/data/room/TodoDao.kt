d060646dd1e5ce11daf350c293522a4abb5ac09c
qpeterp <lsepeter0827@naver.com>
2024-08-21 22:20:01 +0900

    File :: Change todoDao position

---
+++ app/src/main/java/com/qpeterp/todolock/data/room/TodoDao.kt
@@ -0,0 +1,22 @@
+package com.qpeterp.todolock.data.room
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface TodoDao {
+    @Query("SELECT * FROM TodoData")
+    fun getAll(): List<TodoData>
+
+    @Insert
+    fun insertTodo(vararg todo: TodoData)
+
+    @Update
+    fun updateTodo(vararg todo: TodoData)
+
+    @Delete
+    fun deleteTodo(vararg todo: TodoData)
+}
