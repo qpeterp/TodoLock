265cee1b9217c3313e9087641111c11e8330a528
qpeterp <lsepeter0827@naver.com>
2024-08-29 19:32:10 +0900

    Feat :: OverlayView todoList

--- app/src/main/java/com/qpeterp/todolock/specific/lock/OverlayService.kt
+++ app/src/main/java/com/qpeterp/todolock/specific/lock/OverlayService.kt
@@ -10,13 +10,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.WindowManager
 import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.qpeterp.todolock.R
 import com.qpeterp.todolock.common.Constant
+import com.qpeterp.todolock.data.room.TodoData
+import com.qpeterp.todolock.ui.main.lock.CustomAdapter
+import com.qpeterp.todolock.ui.main.todo.TodoViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class OverlayService : Service() {
 
     private lateinit var windowManager: WindowManager
     private lateinit var overlayView: View
+    private lateinit var adapter: CustomAdapter
+    private lateinit var todoViewModel: TodoViewModel
 
     override fun onCreate() {
         super.onCreate()
@@ -39,12 +49,39 @@ class OverlayService : Service() {
             PixelFormat.TRANSLUCENT
         )
 
+        todoViewModel = TodoViewModel(this)
+        initView()
+
         val button = overlayView.findViewById<Button>(R.id.lock_button)
         button.setOnClickListener {
             handleLockState()
         }
 
         windowManager.addView(overlayView, params)
+    }
+
+    private fun initView() {
+        val recyclerView = overlayView.findViewById<RecyclerView>(R.id.recyclerView)
+
+        val todoList: List<TodoData> = todoViewModel.todoList.value
+        Log.d(Constant.TAG, "OverlayService initView: todoList : $todoList")
+
+        adapter = CustomAdapter(todoList)
+
+        // CoroutineScope를 사용하여 비동기적으로 StateFlow를 수집
+        CoroutineScope(Dispatchers.Main).launch {
+            todoViewModel.todoList.collect { todoList ->
+                Log.d(Constant.TAG, "OverlayService initView: todoList : $todoList")
+                adapter.updateData(todoList)  // 어댑터에 새로운 데이터 전달
+            }
+        }
+
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        adapter.itemClick = object : CustomAdapter.ItemClick {
+            override fun onClick(view: View, todoData: TodoData) {}
+        }
     }
 
     private fun handleLockState() {
---
+++ app/src/main/java/com/qpeterp/todolock/ui/main/lock/CustomAdapter.kt
@@ -0,0 +1,44 @@
+package com.qpeterp.todolock.ui.main.lock
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.qpeterp.todolock.data.room.TodoData
+import com.qpeterp.todolock.databinding.ItemTodoBinding
+
+class CustomAdapter(private var todoList : List<TodoData>): RecyclerView.Adapter<CustomAdapter.Holder>() {
+
+    interface ItemClick {  //클릭이벤트추가부분
+        fun onClick(view : View, todoData : TodoData)
+    }
+    var itemClick : ItemClick? = null  //클릭이벤트추가부분
+
+    inner class Holder(binding: ItemTodoBinding): RecyclerView.ViewHolder(binding.root) {
+        val todoContent = binding.todoContent
+        val checkBox = binding.todoCheckBox
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val binding = ItemTodoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return Holder(binding)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.todoContent.text = todoList[position].todo
+
+        holder.checkBox.setOnClickListener {
+            itemClick?.onClick(it, todoList[position])
+        }
+    }
+
+    fun updateData(newTodoList: List<TodoData>) {
+        this.todoList = newTodoList
+        notifyDataSetChanged()  // 데이터가 변경되었음을 어댑터에 알림
+    }
+
+    override fun getItemCount(): Int {
+        return todoList.size
+    }
+}
